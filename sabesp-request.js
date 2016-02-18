c1fd2077df2472cb27ae2ec218c1ed136b6c112a
William Bruno <rocha_bruno@hotmail.com>
2016-02-18 00:29:30 -0200

    last attempt

--- sabesp-request.js
+++ sabesp-request.js
@@ -1,44 +1,52 @@
 'use strict';
 
+const SABESP_HOME = 'https://www9.sabesp.com.br/agenciavirtual/pages/home/paginainicial.iface';
 const SABESP = 'https://www9.sabesp.com.br/agenciavirtual/block/send-receive-updates';
 
 const cheerio = require('cheerio');
 const request = require('request');
 
-const ICE_SESSION = 'IuxsMybtJgrGsupG18NtmA';
-const J_SESSION_ID = '0001apsiLBV_P_yEPyacWZraT1R:19s4dldpu';
-const RGI = '2421964/97'; //194335/90
+const ICE_SESSION = 'rRmMhoMWhh2snlVQM1uRRQ';
+const J_SESSION_ID = '0001nlnDCqp8RmoGCAcWQqhfaJc:19s4dlfap';
+const RGI = '194335/90'; // '2421964/97'; //194335/90
 
 const body = {
-  // 'ice.submit.partial': 'false',
-  // 'ice.event.target': '',
-  // 'ice.event.captured': 'frmhome:j_id163',
-  // 'ice.event.type': 'onclick',
-  // 'ice.event.alt': 'false',
-  // 'ice.event.ctrl': 'false',
-  // 'ice.event.shift': 'false',
-  // 'ice.event.meta': 'false',
-  // 'ice.event.x': '589',
-  // 'ice.event.y': '477',
-  // 'ice.event.left': 'true',
-  // 'ice.event.right': 'false',
-  frmhome: '',
-  icefacesCssUpdates: '',
-  'javax.faces.ViewState': '12',
-  'javax.faces.RenderKitId': '',
-  'frmhome:menuID': '',
-  '': 'pesquisaNumeroRgi',
-  'frmhome:rgi1': RGI,
-  cdMenu: '',
-  outcomeMenu: '',
-  'frmhome:_idcl': 'frmhome:j_id163',
-  outcomeparam: 'PROSSEGUIR',
-  dsMenu: '',
-  'ice.session': ICE_SESSION,
-  // 'ice.view': '12',
-  // 'ice.focus': 'frmhome:j_id163',
-  rand: '0.5068737229265319' };
+  'ice.submit.partial':false,
+  'ice.event.target':'frmhome:botaoNinja',
+  'ice.event.captured':'frmhome:botaoNinja',
+  'ice.event.type':'onclick',
+  'ice.event.alt':false,
+  'ice.event.ctrl':false,
+  'ice.event.shift':false,
+  'ice.event.meta':false,
+  'ice.event.x':0,
+  'ice.event.y':203,
+  'ice.event.left':false,
+  'ice.event.right':false,
+  'frmhome:botaoNinja':'',
+  'dsMenu':'',
+  'outcomeSubMenu':'',
+  'frmhome:_idcl':'',
+  'cdNoticia':'',
+  'outcomeMenu':'',
+  'cdMenu':'',
+  'frmhome:listaFunc':'SUA_CONTA_CONSULTA_HISTORICO_CONTAS',
+  '':'paginainicial',
+  'frmhome:menuID':'0',
+  'javax.faces.RenderKitId':'ICEfacesRenderKit',
+  'javax.faces.ViewState':'1',
+  'icefacesCssUpdates':'',
+  'frmhome':'frmhome',
+  'ice.session':ICE_SESSION,
+  'ice.view':'1',
+  'ice.focus:frmhome':'listaFunc',
+  'rand':0.13521429593674839
+};
 
+
+// request.get(SABESP_HOME, function(err, response) {
+//   console.log(response.body);
+// });
 
 
 var j = request.jar();
@@ -60,9 +68,9 @@ request({
 }, function(err, response) {
   var body = response.body;
 
-  var $ = cheerio.load(body);
+  // var $ = cheerio.load(body);
 
-  console.log($("#frmhome\\:pglogado").html());
+  console.log(body);
 })
 
 
