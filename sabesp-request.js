7f0d7eeaea17a84a3e31bc86e6c410d22ba02cbc
Alisson Carneiro <alisson.carneiro@comitt.com.br>
2016-03-12 23:23:49 -0300

    Adicionando variaveis
    Limpeza de variaveis e teste com dinamismo das variaveis.

--- sabesp-request.js
+++ sabesp-request.js
@@ -5,28 +5,30 @@ const SABESP = 'https://www9.sabesp.com.br/agenciavirtual/block/send-receive-upd
 
 const cheerio = require('cheerio');
 const request = require('request');
+const Promise = require('bluebird');
 
-const ICE_SESSION = 'rRmMhoMWhh2snlVQM1uRRQ';
-const J_SESSION_ID = '0001nlnDCqp8RmoGCAcWQqhfaJc:19s4dlfap';
+const ICE_SESSION = 'ufqJDq03NMNrF0CzZbiFYs';
+const J_SESSION_ID = '0003iG3dFm2476NqGo1xhDYcWqq:18bh9jn9d';
 const RGI = '194335/90'; // '2421964/97'; //194335/90
+var rand = Math.random();
+var VIEW = 49;
+var CLICKHOME = 'frmhome:logoTopo';
+var CLICKBTNNINJA = 'frmhome:botaoNinja';
+
+
 
 const body = {
   'ice.submit.partial':false,
-  'ice.event.target':'frmhome:botaoNinja',
-  'ice.event.captured':'frmhome:botaoNinja',
+
+
+  'ice.event.target':CLICKBTNNINJA,
+  'ice.event.captured':CLICKBTNNINJA,
+  'frmhome:_idcl': '',
+  
   'ice.event.type':'onclick',
-  'ice.event.alt':false,
-  'ice.event.ctrl':false,
-  'ice.event.shift':false,
-  'ice.event.meta':false,
-  'ice.event.x':0,
-  'ice.event.y':203,
-  'ice.event.left':false,
-  'ice.event.right':false,
   'frmhome:botaoNinja':'',
   'dsMenu':'',
   'outcomeSubMenu':'',
-  'frmhome:_idcl':'',
   'cdNoticia':'',
   'outcomeMenu':'',
   'cdMenu':'',
@@ -34,14 +36,14 @@ const body = {
   '':'paginainicial',
   'frmhome:menuID':'0',
   'javax.faces.RenderKitId':'ICEfacesRenderKit',
-  'javax.faces.ViewState':'1',
+  'javax.faces.ViewState':VIEW,
   'icefacesCssUpdates':'',
   'frmhome':'frmhome',
   'ice.session':ICE_SESSION,
-  'ice.view':'1',
+  'ice.view':VIEW,
   'ice.focus:frmhome':'listaFunc',
-  'rand':0.13521429593674839
-};
+  'rand':rand
+  };
 
 
 // request.get(SABESP_HOME, function(err, response) {
@@ -49,28 +51,36 @@ const body = {
 // });
 
 
+
 var j = request.jar();
 var cookie = request.cookie('JSESSIONID='+J_SESSION_ID+';ice.sessions='+ICE_SESSION);
 j.setCookie(cookie, SABESP);
 
-request({
-  url: SABESP,
-  method: 'POST',
-  headers: {
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*-/*;q=0.8',
-    'Host': 'www9.sabesp.com.br',
-    'Origin': 'http://www9.sabesp.com.br',
-    'Referer': 'https://www9.sabesp.com.br/agenciavirtual/pages/home/paginainicial.iface',
-    'User-Agent': 'Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/30.0.1599.114 Safari/537.36',
-  },
-  jar: j,
-  form: body
-}, function(err, response) {
-  var body = response.body;
 
-  // var $ = cheerio.load(body);
 
-  console.log(body);
-})
+
+	request({
+	  url: SABESP,
+	  method: 'POST',
+	  headers: {
+		'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*-/*;q=0.8',
+		'Host': 'www9.sabesp.com.br',
+		'Origin': 'http://www9.sabesp.com.br',
+		'Referer': 'https://www9.sabesp.com.br/agenciavirtual/pages/home/paginainicial.iface',
+		'User-Agent': 'Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/30.0.1599.114 Safari/537.36',
+	  },
+	  jar: j,
+	  form: body
+	}, function(err, response) {
+
+
+		var body = response.body;
+
+		  var $ = cheerio.load(body);
+
+			console.log($.html());
+		//  console.log($('.contaContent').contents().length);
+		  //console.log($('.contaContent').text());
+	})
 
 
